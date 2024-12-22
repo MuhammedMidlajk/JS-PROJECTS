a875ab57e5c93abee2ece11b4faa9a6bc69b9a5f
MuhammedMidlajk <midlajmuhamd@gmial.com>
2024-12-22 13:06:16 +0530

    CLICK-BUTTON

---
+++ click-button/first.js
@@ -0,0 +1,19 @@
+
+
+var a=0
+function myFunction(){
+    a++
+    
+    const box = document.getElementById("box");
+
+    if(a==1){
+        box.style.backgroundColor = "#6A9C89" 
+    }
+    else {
+        box.style.backgroundColor = "#4A628A"
+    }
+    
+   
+}
+
+
