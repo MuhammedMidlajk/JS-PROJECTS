6c7de9044c291b4e53e1aaadf7276bf4d218b055
MuhammedMidlajk <midlajmuhamd@gmial.com>
2024-12-23 10:00:10 +0530

    CLICK-BUTTON-2

---
+++ click-button-2/second.js
@@ -0,0 +1,40 @@
+var a=0
+function myfunction(){
+a++
+
+const touch = document.getElementById("touch");
+
+    if(a==1){
+        touch.style.backgroundColor = "#A8CD89";
+    }
+    
+}
+
+
+let b=0
+function colorChange(){
+b++
+
+const touchTwo = document.getElementById("touchTwo");
+
+if(b==1){
+    touchTwo.style.backgroundColor = "#FF0000"
+}
+    
+
+}
+
+
+var c=0
+function changeColor(){
+    c++
+
+    const touchThree = document.getElementById("touchThree")
+
+    if(c==1){
+
+        touchThree.style.backgroundColor = "#FF0000"
+    }
+
+
+}
